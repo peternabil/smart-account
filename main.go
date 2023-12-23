f764ef8c44070adc1493f4b0680d1f81de7ce54a
Peter Nabil <peter.cypod@gmail.com>
2023-12-23 13:06:54 +0200

    fix for release

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/lpernett/godotenv"
 	"github.com/peternabil/go-api/controllers"
 	"github.com/peternabil/go-api/intitializers"
+	"github.com/peternabil/go-api/models"
 )
 
 func init() {
@@ -14,11 +15,32 @@ func init() {
 	intitializers.LoadDB()
 }
 
+func migrate() {
+	var err error
+	err = intitializers.DB.AutoMigrate(&models.Category{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = intitializers.DB.AutoMigrate(&models.Priority{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = intitializers.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = intitializers.DB.AutoMigrate(&models.Transaction{})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
 func main() {
 	godotenv.Load()
 	var store intitializers.MainStore
 	server, _ := controllers.NewServer(store)
 	intitializers.LoadDB()
+	migrate()
 	server.Start(fmt.Sprintf(
 		"%s:%s",
 		os.Getenv("API_ADDRESS"),
