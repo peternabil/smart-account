d71a4db8e0fcb05812fe9a8d99e71e951fba64de
Peter Nabil <peter.cypod@gmail.com>
2023-12-25 01:47:43 +0200

    fix cert

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365
+openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
+    -subj "/C=EG/ST=Cairo/L=Cairo/O=Peter/OU=IT Department/CN=localhost"
 
 # docker build . -t my_app
 
