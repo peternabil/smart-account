8d41e144f7a7c672e0b13f8b5cd61d9ec1066102
Peter Nabil <peter.cypod@gmail.com>
2023-12-25 01:35:18 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
-    -subj "/C=GB/ST=London/L=London/O=Alros/OU=IT Department/CN=localhost"
+openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365
 
 # docker build . -t my_app
 
